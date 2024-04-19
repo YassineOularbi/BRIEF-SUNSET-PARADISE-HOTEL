e2fb43901faecd3cfba59fb0ae78384fc0657d89
YassineOularbi <yassineoularbi4@gmail.com>
2024-04-19 15:58:17 +0100

    Add part of front end in jsp file

--- src/main/java/com/java/servlets/Test.java
+++ src/main/java/com/java/servlets/Test.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.java.dao.RoomDAOImpl;
 
 
-@WebServlet("/Home")
+@WebServlet("/Test")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
